297d19cf841ee03b5a9fcbbc43fc5899374db131
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2024-08-22 18:15:36 +0300

    constructor

--- UnitTest.APP/xUnitTest.Test/CalculatorTest.cs
+++ UnitTest.APP/xUnitTest.Test/CalculatorTest.cs
@@ -4,13 +4,19 @@ namespace xUnitTest.Test
 {
     public class CalculatorTest
     {
+
+         public Calculator calculator { get; set; }
+        public CalculatorTest()
+        {
+                this.calculator = new Calculator();
+        }
+
         // Test işlemi 3 evreden olusur: Arrange,Act,Assert
         [Fact]
         public void AddTest() {
 
             ////Arrange : Degiskenlerin initialize edildigi yer bu kisimdir veya nesne örnegi olusturulan kisim
             //int a =5; int b = 10;
-            //var calculator = new Calculator();
 
             ////Act
             //var total = calculator.add(a, b);
