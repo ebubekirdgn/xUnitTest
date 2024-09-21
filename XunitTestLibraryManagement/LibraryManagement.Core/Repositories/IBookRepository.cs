73f1398ac4ba651001292212c17047371a3f62ad
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2024-09-22 01:42:33 +0300

    cleanup all code lines

--- XunitTestLibraryManagement/LibraryManagement.Core/Repositories/IBookRepository.cs
+++ XunitTestLibraryManagement/LibraryManagement.Core/Repositories/IBookRepository.cs
@@ -5,7 +5,9 @@ namespace LibraryManagement.Core.Repositories
     public interface IBookRepository
     {
         Task AddAsync(Book book);
+
         Task RemoveAsync(int id);
+
         Task<IEnumerable<Book>> GetAllAsync();
     }
 }
